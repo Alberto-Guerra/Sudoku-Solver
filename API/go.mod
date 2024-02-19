52fc345cc3e14062dd8fced7b1f0f8d507e05bf4
Alberto-Guerra <albertoguerrarodas@gmail.com>
2024-02-19 16:36:54 +0100

    API created and connected

--- API/go.mod
+++
@@ -1,3 +0,0 @@
-module SudokuSolver
-
-go 1.22.0
--- SudokuSolverUI/src/app/app.component.ts
+++ SudokuSolverUI/src/app/app.component.ts
@@ -23,18 +23,31 @@ export class AppComponent {
   ];
 
   solveSudoku() {
-    this.sudokuGrid = [
-      [5, 3, 2, 4, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 3, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    ]
-    //this.solve(this.sudokuGrid);
+
+    this.solve(this.sudokuGrid);
+    /*const apiUrl = 'https://localhost:7160/sudoku/getsudoku';
+    return fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Assuming the API response contains the solved sudoku grid
+        if (data) {
+          this.sudokuGrid = data;
+          return true;
+        } else {
+          console.log("No solution exists");
+          return false;
+        }
+      })
+      .catch(error => {
+        console.error("Error solving sudoku:", error);
+        return false;
+      });
+    */
   }
   checkValue(input : any) {
     if (input.value == 0) {
@@ -42,9 +55,8 @@ export class AppComponent {
     }
   }
   solve(sudokuGrid: number[][]) {
-    const apiUrl = 'https://api.example.com/solve-sudoku';
-    const requestBody = { sudokuGrid };
-
+    const apiUrl = 'https://localhost:7160/sudoku/solvesudoku';
+    const requestBody = sudokuGrid ;
     return fetch(apiUrl, {
       method: 'POST',
       headers: {
@@ -55,8 +67,8 @@ export class AppComponent {
       .then(response => response.json())
       .then(data => {
         // Assuming the API response contains the solved sudoku grid
-        if (data && data.solvedGrid) {
-          this.sudokuGrid = data.solvedGrid;
+        if (data) {
+          this.sudokuGrid = data;
           return true;
         } else {
           console.log("No solution exists");

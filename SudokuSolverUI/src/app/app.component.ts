9ed0a46c39b99e693bc53e91f3f2cb39e9201d72
Alberto-Guerra <albertoguerrarodas@gmail.com>
2024-02-20 15:22:18 +0100

    UI Sudoku fix

--- SudokuSolverUI/src/app/app.component.ts
+++ SudokuSolverUI/src/app/app.component.ts
@@ -1,87 +1,97 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   title = 'SudokuSolverUI';
-  sudokuGrid: number[][] =
-  [
-    [5, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 4, 0, 0, 0, 0, 6, 0, 7],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 3, 0, 0, 0],
-    [0, 0, 0, 0, 2, 0, 0, 0, 0],
-    [0, 0, 0, 5, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 8, 0],
-    [9, 0, 0, 0, 0, 0, 0, 0, 4],
-    [0, 0, 6, 0, 0, 0, 0, 0, 0]
+  sudokuGrid: string[][] = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
   ];
-  sudokuInput: string = JSON.stringify(this.sudokuGrid);
+
+  sudokuInput: string = JSON.stringify(this.getSudokuNumbers());
   originalSudokuGrid?: number[][];
 
-  solveSudoku() {
-    this.originalSudokuGrid = this.sudokuGrid.map((row) => {
-      return row.slice();
+
+  getSudokuNumbers() : number[][] {
+    return this.sudokuGrid.map((row) => {
+      return row.map((cell) => {
+        return cell === ' ' ? 0 : parseInt(cell);
+      });
     });
-    this.solve(this.sudokuGrid);
-    
   }
 
-  update(sudoku : string){
+  setSudokuNumbers(sudokuNumbers: number[][]) {
+    this.sudokuGrid = sudokuNumbers.map((row) => {
+      return row.map((cell) => {
+        return cell === 0 ? ' ' : cell.toString();
+      });
+    });
+  }
+
+  solveSudoku() {
+    this.originalSudokuGrid = this.getSudokuNumbers().map((row) => {
+      return row.slice();
+    });
+    this.solve(this.getSudokuNumbers());
+  }
+
+  update(sudoku: string) {
     this.sudokuGrid = JSON.parse(sudoku);
   }
 
   clean() {
     this.sudokuGrid = this.sudokuGrid.map((row) => {
-      return row.map(() => 0);
+      return row.map(() => {
+        return ' ';
+      });
     });
     this.originalSudokuGrid = undefined;
   }
-  
+
   solve(sudokuGrid: number[][]) {
+    console.log('Solving sudoku:', sudokuGrid);
     const apiUrl = 'https://localhost:7160/sudoku/solvesudoku';
-    const requestBody = sudokuGrid ;
+    const requestBody = sudokuGrid;
     return fetch(apiUrl, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(requestBody)
+      body: JSON.stringify(requestBody),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         // Assuming the API response contains the solved sudoku grid
         if (data) {
-          console.log("Sudoku solved successfully");
-          this.sudokuGrid = data;
+          console.log('Sudoku solved successfully');
+          this.setSudokuNumbers(data);
           return true;
         } else {
-          console.log("No solution exists");
+          console.log('No solution exists');
           return false;
         }
       })
-      .catch(error => {
-        console.error("Error solving sudoku:", error);
+      .catch((error) => {
+        console.error('Error solving sudoku:', error);
         return false;
       });
   }
 
-  getSudokuValue(row: number, col: number) {
-    let value = this.sudokuGrid[row][col];
-    return value === 0 ? '' : value;
-  }
-
   isOriginal(row: number, col: number) {
-    if(this.originalSudokuGrid){
+    if (this.originalSudokuGrid) {
       return this.originalSudokuGrid[row][col] !== 0;
     }
     return false;
-    
   }
- 
 }

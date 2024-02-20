da53fa64924b480a211a6f763e6e388fb22314c1
Alberto-Guerra <albertoguerrarodas@gmail.com>
2024-02-20 13:37:25 +0100

    Import Sudoku from Json

--- SudokuSolverUI/src/app/app.component.ts
+++ SudokuSolverUI/src/app/app.component.ts
@@ -11,49 +11,38 @@ export class AppComponent {
   title = 'SudokuSolverUI';
   sudokuGrid: number[][] =
   [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 3, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    [5, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 4, 0, 0, 0, 0, 6, 0, 7],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 0, 0, 5, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [9, 0, 0, 0, 0, 0, 0, 0, 4],
+    [0, 0, 6, 0, 0, 0, 0, 0, 0]
   ];
+  sudokuInput: string = JSON.stringify(this.sudokuGrid);
+  originalSudokuGrid?: number[][];
 
   solveSudoku() {
-
+    this.originalSudokuGrid = this.sudokuGrid.map((row) => {
+      return row.slice();
+    });
     this.solve(this.sudokuGrid);
-    /*const apiUrl = 'https://localhost:7160/sudoku/getsudoku';
-    return fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Assuming the API response contains the solved sudoku grid
-        if (data) {
-          this.sudokuGrid = data;
-          return true;
-        } else {
-          console.log("No solution exists");
-          return false;
-        }
-      })
-      .catch(error => {
-        console.error("Error solving sudoku:", error);
-        return false;
-      });
-    */
+    
   }
-  checkValue(input : any) {
-    if (input.value == 0) {
-        input.value = '';
-    }
+
+  update(sudoku : string){
+    this.sudokuGrid = JSON.parse(sudoku);
   }
+
+  clean() {
+    this.sudokuGrid = this.sudokuGrid.map((row) => {
+      return row.map(() => 0);
+    });
+    this.originalSudokuGrid = undefined;
+  }
+  
   solve(sudokuGrid: number[][]) {
     const apiUrl = 'https://localhost:7160/sudoku/solvesudoku';
     const requestBody = sudokuGrid ;
@@ -68,6 +57,7 @@ export class AppComponent {
       .then(data => {
         // Assuming the API response contains the solved sudoku grid
         if (data) {
+          console.log("Sudoku solved successfully");
           this.sudokuGrid = data;
           return true;
         } else {
@@ -84,6 +74,14 @@ export class AppComponent {
   getSudokuValue(row: number, col: number) {
     let value = this.sudokuGrid[row][col];
     return value === 0 ? '' : value;
+  }
+
+  isOriginal(row: number, col: number) {
+    if(this.originalSudokuGrid){
+      return this.originalSudokuGrid[row][col] !== 0;
+    }
+    return false;
+    
   }
  
 }
